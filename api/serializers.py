7e7c6af89e1ac89ae4ce3807fd252ced5d5aca6f
djumanov <diyorbekjumanov1120@gmail.com>
2023-08-08 09:29:00 +0500

    serializers

---
+++ api/serializers.py
@@ -0,0 +1,11 @@
+from rest_framework import serializers
+from .models import Task
+from datetime import datetime
+
+
+class TaskSerializer(serializers.Serializer):
+    id = serializers.IntegerField(read_only=True)
+    title = serializers.CharField(max_length=200)
+    description = serializers.CharField(allow_null=True)
+    completed = serializers.BooleanField(default=False)
+    created = serializers.DateTimeField(default=datetime.now())
--- api/views.py
+++ api/views.py
@@ -5,13 +5,12 @@ from rest_framework import status
 from django.forms.models import model_to_dict
 
 from .models import Task
+from .serializers import TaskSerializer
 
 
 @api_view(['GET'])
 def task_list(request: Request):
-    tasks = Task.objects.all()
-    data = []
-    for task in tasks:
-        data.append(model_to_dict(task))
-    return Response(data=data, status=status.HTTP_200_OK)
+    task = Task.objects.first()
+    serializer = TaskSerializer(task)
+    return Response(data=serializer.data, status=status.HTTP_200_OK)
 
